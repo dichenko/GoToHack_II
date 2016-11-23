5d950b88ce58fdcce3a91b67b0d4580a0c8f8ec2
dichenko <m@dichenko.ru>
2016-11-23 22:14:01 +0300

    GotoHack 2

--- task_02/02_start.py
+++ task_02/02_start.py
@@ -73,8 +73,20 @@ for key in user_dict:
 for key in user_dict:
     for i in range(len(user_dict[key]) - 1):
         if user_dict[key][i] == user_dict[key][i+1]:
-            user_dict[key][i] = None #FIXME!!!!!!! 'NoneType' object is not subscriptable
+            user_dict[key][i] = None
 
+for key in user_dict:
+    z = user_dict[key]
+    for el in z:
+        if el == None:
+            z.remove(el)
+
+user_dict[63].remove(None)
+
+for el in user_dict:
+    for k in el:
+        if k == None:
+            print(el, k, 'ACHTUNG!!!!!!')
 
 # Создаем словарь, где для каждого fake_step_id будет значением количество пользователей,
 #которое сделало возврат к этому степу vozvrat = {fake_step_id: val}
@@ -84,17 +96,23 @@ vozvrat = {}
 #для каждого пользователя ищем степы с возвратом
 #
 for key in user_dict:  #Для каждого пользователя
+    print(key)
+    w = user_dict[key]
     passed_step_id = []  #Заводим список
-    for i in range(len(user_dict[key])): #По количеству записей
-        fid = user_dict[key][i][3]
+    for i in range(len(w)): #По количеству записей
+        q = w[i]
+        fid = q[3]
         if fid not in passed_step_id:  #Если ещё не находили такой возвратный fid, работаем
             #Проходим по всему списку и ищем следующий степ
-            for j in range(len(user_dict[key])):
+            for j in range(len(w)):
+                p = w[j]
                 #Если мы нашли следующий степ с бОльшим временем
-                if (user_dict[key][j][3] == fid+1) and (user_dict[key][j][2] > user_dict[key][i][2]):
+                if (p[3] == fid + 1) and (p[2] > q[2]):
                     #Ищем fid с ещё бОльшим временем
-                    for z in range(len(user_dict[key])):
-                        if user_dict[key][z][3] == fid and user_dict[key][z][2] > user_dict[key][j][2]:
+                    for z in range(len(w)):
+                        m = w[z]
+
+                        if m[3] == fid and m[2] > p[2]:
 
                             if fid not in passed_step_id:
                                 #print('user:', key, 'f_step_id:', fid)
