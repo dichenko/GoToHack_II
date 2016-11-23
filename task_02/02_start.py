545848f3493dc5ebb2cc3dd3ff0cd34c8f85bff4
dichenko <m@dichenko.ru>
2016-11-23 10:41:49 +0300

    nti

--- task_02/02_start.py
+++ task_02/02_start.py
@@ -64,8 +64,64 @@ for i in range(len(data)):
     else:
         user_dict[data[i][0]] = [data[i][1:]]
 
+
+#Сортируем список событий каждого пользователя по времени
 for key in user_dict:
-    print(key, user_dict[key])
+    user_dict[key].sort(key=lambda x: x[2])
+
+#Проходим по каждому списку событий и удаляем из него события с одинаковым временем
+for key in user_dict:
+    for i in range(len(user_dict[key]) - 1):
+        if user_dict[key][i] == user_dict[key][i+1]:
+            user_dict[key][i] = None
+
+
+# Создаем словарь, где для каждого fake_step_id будет значением количество пользователей,
+#которое сделало возврат к этому степу vozvrat = {fake_step_id: val}
+
+vozvrat = {}
+
+
+#для каждого пользователя ищем степы с возвратом
+#
+for key in user_dict:  #Для каждого пользователя
+    passed_step_id = []  #Заводим список
+    for i in range(len(user_dict[key])): #По количеству записей
+        fid = user_dict[key][i][3]
+        if fid not in passed_step_id:  #Если ещё не находили такой возвратный fid, работаем
+            #Проходим по всему списку и ищем следующий степ
+            for j in range(len(user_dict[key])):
+                #Если мы нашли следующий степ с бОльшим временем
+                if (user_dict[key][j][3] == fid+1) and (user_dict[key][j][2] > user_dict[key][i][2]):
+                    #Ищем fid с ещё бОльшим временем
+                    for z in range(len(user_dict[key])):
+                        if user_dict[key][k][3] == fid and user_dict[key][k][2] > user_dict[key][j][2]:
+                            passed_step_id.append(fid) #Добавляем fid  в список рассмотренных
+                            if fid in vozvrat.keys():
+                                vozvrat[fid] += 1
+                            else:
+                                vozvrat[fid] = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for key in user_dict:
+    print(key,':_____________________________' )
+    for el in user_dict[key]:
+        print(el)
+
 
 
 
